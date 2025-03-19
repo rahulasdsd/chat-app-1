b1b93dac0bff8efe5221b7f3fd4ee104546bcd8d
rahulasdsd <166151529+rahulasdsd@users.noreply.github.com>
2025-03-19 21:47:10 +0530

    Update app.js

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.use(express.static(path.join(__dirname, "front-end", "build")));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.json());
 
-const corsOptions = { origin: "http://localhost:3000" };
+const corsOptions = { origin: "http://localhost:3001" };
 app.use(cors(corsOptions));
 
 mongoose
